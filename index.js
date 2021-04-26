48d019361811cbe767f004ca28dce5e698b8619a
davekolian <50661033+davekolian@users.noreply.github.com>
2021-04-27 01:02:35 +0400

    Added navigation bar when scrolling + changed the logo to a svg

---
+++ index.js
@@ -0,0 +1,35 @@
+function vh(v) {
+  var h = Math.max(
+    document.documentElement.clientHeight,
+    window.innerHeight || 0
+  );
+  return (v * h) / 100;
+}
+
+function vw(v) {
+  var w = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+  return (v * w) / 100;
+}
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= vh(12)) {
+    $("nav").addClass("fixed-header");
+    $("nav").removeClass("nav_on_top");
+  } else {
+    $("nav").removeClass("fixed-header");
+    $("nav").addClass("nav_on_top");
+  }
+});
+
+function clickSingleA(a) {
+  items = document.querySelectorAll(".active"); //should be ".single.active"
+
+  if (items.length) {
+    items[0].className = "single";
+  }
+
+  a.className = "single active";
+}
