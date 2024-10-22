39f39820b1e56e36d86e44e9902b53f4e8d87291
davekolian <50661033+davekolian@users.noreply.github.com>
2024-10-22 22:41:16 +0400

    Created new changes to the entire layout, including the main welcome page, added a selector to choose projects from different paths

--- index.js
+++ index.js
@@ -1,72 +1,89 @@
 function vh(v) {
-  var h = Math.max(
-    document.documentElement.clientHeight,
-    window.innerHeight || 0
-  );
-  return (v * h) / 100;
+	var h = Math.max(
+		document.documentElement.clientHeight,
+		window.innerHeight || 0
+	);
+	return (v * h) / 100;
 }
 
 function vw(v) {
-  var w = Math.max(
-    document.documentElement.clientWidth,
-    window.innerWidth || 0
-  );
-  return (v * w) / 100;
+	var w = Math.max(
+		document.documentElement.clientWidth,
+		window.innerWidth || 0
+	);
+	return (v * w) / 100;
 }
 
 $(window).scroll(function () {
-  var nav_bar_height = $("nav").height();
-  var galleria_height = $("#galleria").height();
-  var code_works_height = $("#code_works").height();
-  var welcome_height = $("#welcome_page").height();
-  if ($(window).scrollTop() >= nav_bar_height) {
-    $("nav").addClass("fixed-header");
-    $("nav").removeClass("nav_on_top");
-  } else {
-    $("nav").removeClass("fixed-header");
-    $("nav").addClass("nav_on_top");
-  }
+	var nav_bar_height = $('nav').height();
+	var galleria_height = $('#galleria').height();
+	var code_works_height = $('#code_works').height();
+	var welcome_height = $('#welcome_page').height();
+	if ($(window).scrollTop() >= nav_bar_height) {
+		$('nav').addClass('fixed-header');
+		$('nav').removeClass('nav_on_top');
+	} else {
+		$('nav').removeClass('fixed-header');
+		$('nav').addClass('nav_on_top');
+	}
 
-  if ($(window).scrollTop() < code_works_height) {
-    $(".code_works").removeClass("active");
-    $(".galleria").removeClass("active");
-    $(".contact_me").removeClass("active");
-  } else if (
-    $(window).scrollTop() >= nav_bar_height + welcome_height - 100 &&
-    $(window).scrollTop() <
-      nav_bar_height + welcome_height + code_works_height - 100
-  ) {
-    $(".code_works").addClass("active");
-    $(".galleria").removeClass("active");
-    $(".contact_me").removeClass("active");
-  } else if (
-    $(window).scrollTop() >=
-      nav_bar_height + welcome_height + code_works_height - 100 &&
-    $(window).scrollTop() + $(window).height() < $(document).height() - 100
-  ) {
-    $(".code_works").removeClass("active");
-    $(".galleria").addClass("active");
-    $(".contact_me").removeClass("active");
-  } else if (
-    $(window).scrollTop() + $(window).height() >=
-    $(document).height() - 100
-  ) {
-    $(".code_works").removeClass("active");
-    $(".galleria").removeClass("active");
-    $(".contact_me").addClass("active");
-  }
+	if ($(window).scrollTop() < code_works_height) {
+		$('.code_works').removeClass('active');
+		$('.galleria').removeClass('active');
+		$('.contact_me').removeClass('active');
+	} else if (
+		$(window).scrollTop() >= nav_bar_height + welcome_height - 100 &&
+		$(window).scrollTop() <
+			nav_bar_height + welcome_height + code_works_height - 100
+	) {
+		$('.code_works').addClass('active');
+		$('.galleria').removeClass('active');
+		$('.contact_me').removeClass('active');
+	} else if (
+		$(window).scrollTop() >=
+			nav_bar_height + welcome_height + code_works_height - 100 &&
+		$(window).scrollTop() + $(window).height() < $(document).height() - 100
+	) {
+		$('.code_works').removeClass('active');
+		$('.galleria').addClass('active');
+		$('.contact_me').removeClass('active');
+	} else if (
+		$(window).scrollTop() + $(window).height() >=
+		$(document).height() - 100
+	) {
+		$('.code_works').removeClass('active');
+		$('.galleria').removeClass('active');
+		$('.contact_me').addClass('active');
+	}
 });
 
 function clickSingleA(a) {
-  // items = document.querySelectorAll(".active"); //should be ".single.active"
+	// items = document.querySelectorAll(".active"); //should be ".single.active"
 
-  // if (items.length) {
-  //   items[0].className = "single";
-  // }
+	// if (items.length) {
+	//   items[0].className = "single";
+	// }
 
-  a.addClass("active");
+	a.addClass('active');
 }
 
 function openProject(link) {
-  window.open(link, "mywindow");
+	window.open(link, 'mywindow');
+}
+
+function activeProjectsSelector(button, box) {
+	buttons = document.getElementsByClassName('ps_button');
+
+	for (let i = 0, len = buttons.length; i < len; i++) {
+		buttons[i].classList.remove("active");
+	}
+
+	button.classList.add('active');
+	projects = document.getElementsByClassName('projects');
+
+	for (let i = 0, len = projects.length; i < len; i++) {
+		projects[i].style.display = 'none';
+	}
+
+	document.getElementById(box.id).style.display = 'grid';
 }
